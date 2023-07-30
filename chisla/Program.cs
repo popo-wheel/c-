fa29bf4177de1580c1d4a20a9800744eace8a637
Petya <petya200606@gmail.com>
2023-07-31 00:40:17 +0600

    Chisla random

--- chisla/Program.cs
+++ chisla/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 3;
-int numberB = 7;
-Console.WriteLine(numberA + numberB);
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.Write(numberA);
+int numberB = new Random().Next(1, 10);
+Console.Write(numberB);
+int result = numberA + numberB;
+Console.Write(result);
