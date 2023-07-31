da3458e07ce1eae4d8a4b74d31f176c3bac719da
Petya <petya200606@gmail.com>
2023-07-31 21:59:41 +0600

    Wow its a Timur

---
+++ Hello Timur/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Add your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "timur")
+{
+    Console.WriteLine("Wow, its Timur!");
+}
+else{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
